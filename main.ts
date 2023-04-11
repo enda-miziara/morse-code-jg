941b4e5410859ec726fa785f4ef555535ac54861
enda-miziara <128720530+enda-miziara@users.noreply.github.com>
2023-04-11 12:17:43 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 input.onButtonPressed(Button.A, function () {
     step += 1
+    basic.showString("" + (step))
 })
 input.onButtonPressed(Button.AB, function () {
 	
 })
 input.onButtonPressed(Button.B, function () {
-    listindex = listnumber.indexOf(step)
-    basic.showNumber(listindex)
+    StepText = convertToText(step)
+    listindex = listnumber.indexOf(StepText)
+    basic.showString("" + (listindex))
 })
+let StepText = ""
 let listindex = 0
 let step = 0
 let listnumber: string[] = []
