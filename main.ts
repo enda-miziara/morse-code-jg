1c619933506a9bb7b43820034d582107ee125345
enda-miziara <128720530+enda-miziara@users.noreply.github.com>
2023-04-23 16:11:17 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,32 @@
 input.onButtonPressed(Button.A, function () {
     step += 1
-    basic.showString("" + step)
+    basic.showNumber(step)
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(step)
+    StepText = convertToText(step)
+    radio.sendString(StepText)
 })
 radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+    listindex = listnumber.indexOf(receivedString)
+    basic.showString("" + (listtext[listindex]))
 })
 input.onButtonPressed(Button.B, function () {
-    StepText = convertToText(step)
-    basic.showString("" + listindex)
+	
 })
 let StepText = ""
 let listindex = 0
 let step = 0
-let listnumber = [
-"1",
+let listtext: string[] = []
+let listnumber: string[] = []
+listnumber = [
+"5",
+"7",
 "2",
-"3",
-"4",
-"5"
+"8",
+"1"
 ]
 radio.sendNumber(0)
-let listtext = [
+listtext = [
 "a",
 "b",
 "c",
